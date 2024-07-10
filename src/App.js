a885f4ffe9b30a177f2e5f42ca329b3143846e01
shrikant-kushwah <shrikant20052001@gmail.com>
2024-07-10 16:14:41 +0530

    new updated1.0

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Service from "./components/Service";
+// import Service from "./components/Service";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 
