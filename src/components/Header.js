c84bb3d19d1e6ef21e6e9bf7ee64886167d423f1
Shrikant Kushwah <96473205+shrikant-kushwah@users.noreply.github.com>
2024-08-18 02:40:01 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -64,7 +64,7 @@ const Header = () => {
             <a className='flex justify-between items-center w-full text-gray-300' href="mailto:shrikant@20052001@gmail.com">Email <HiOutlineMail size={30} /></a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a className='flex justify-between items-center w-full text-gray-300' href="https://drive.google.com/file/d/1sLmhxsXmenGmhH-Bbxin6TzFJh9nK6oo/view?usp=sharing">Resume <BsFillPersonLinesFill size={30} /></a>
+            <a className='flex justify-between items-center w-full text-gray-300' href="https://drive.google.com/file/d/1OdH8ai8WtYxTl5yTENGUT4Gxe1HZzdV5/view?usp=sharing">Resume <BsFillPersonLinesFill size={30} /></a>
           </li>
         </ul>
       </div>
